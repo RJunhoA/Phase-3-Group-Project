f34cbf183e941b702c23b1f01ce8bb966dce268e
Raymond An <raymond_an@yahoo.com>
2023-03-27 10:34:35 -0400

    made something work

---
+++ lib/cli.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column("id", Integer, primary_key=True)
+    first_name = Column("firstname", String)
+    last_name = Column("lastname", String)
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name}"
+
+engine = create_engine("sqlite:///mydb.db", echo=True)
+Base.metadata.create_all(bind=engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+user = User("Billy", "Bob")
+session.add(user)
+session.commit()
